437d81f1dedebbf6279195eaa914c608f188319e
belforz <macedobeiramar@hotmail.com>
2026-05-01 18:00:08 -0300

    feat: implement vaccination repository

---
+++ src/main/java/com/example/imunidata/repository/RegistroVacinacaoRepository.java
@@ -0,0 +1,18 @@
+package com.example.imunidata.repository;
+
+import com.example.imunidata.model.RegistroVacinacao;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface RegistroVacinacaoRepository extends JpaRepository<RegistroVacinacao, Long> {
+
+    List<RegistroVacinacao> findByVacinaIgnoreCase(String vacina);
+
+    List<RegistroVacinacao> findByEstadoIgnoreCase(String estado);
+
+    List<RegistroVacinacao> findByVacinaIgnoreCaseAndEstadoIgnoreCase(String vacina, String estado);
+}
+
