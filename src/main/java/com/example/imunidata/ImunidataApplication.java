e4b9e41f7d1840b0813dbb8bf7fb78ea8be8a7d8
belforz <macedobeiramar@hotmail.com>
2026-05-01 17:08:11 -0300

    feature: add new maven dependencies

---
+++ src/main/java/com/example/imunidata/ImunidataApplication.java
@@ -0,0 +1,13 @@
+package com.example.imunidata;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ImunidataApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ImunidataApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/imunidata/ImunidataApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.imunidata;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ImunidataApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
