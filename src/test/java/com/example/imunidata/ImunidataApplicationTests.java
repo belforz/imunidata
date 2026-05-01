6e04af5d623d7028157f3a3eafffca57528da651
belforz <macedobeiramar@hotmail.com>
2026-05-01 18:36:58 -0300

    feature: add csv data

--- src/test/java/com/example/imunidata/ImunidataApplicationTests.java
+++ src/test/java/com/example/imunidata/ImunidataApplicationTests.java
@@ -1,13 +1,11 @@
 package com.example.imunidata;
 
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
 class ImunidataApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    void contextLoads() {
+    }
 
 }
