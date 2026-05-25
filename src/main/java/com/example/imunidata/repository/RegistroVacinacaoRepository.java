59cc7da1faa4386be078c15952bc9fb25ca336d8
belforz <macedobeiramar@hotmail.com>
2026-05-24 22:11:14 -0300

    refactor: change repository limits

--- src/main/java/com/example/imunidata/repository/RegistroVacinacaoRepository.java
+++ src/main/java/com/example/imunidata/repository/RegistroVacinacaoRepository.java
@@ -4,17 +4,7 @@ import com.example.imunidata.model.RegistroVacinacao;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import java.util.List;
-
 @Repository
 public interface RegistroVacinacaoRepository extends JpaRepository<RegistroVacinacao, Long> {
-
-    List<RegistroVacinacao> findByVacina(String vacina);
-
-    List<RegistroVacinacao> findByEstado(String estado);
-
-    List<RegistroVacinacao> findByVacinaAndEstado(String vacina, String estado);
-
-
-    boolean existsByVacinaAndEstadoAndMunicipioAndDose(String vacina, String estado, String municipio, String dose);
+    // Removi atuacao da camada Repository devido aos arquivos longos
 }
